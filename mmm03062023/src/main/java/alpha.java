214d7ed980342402591fa69f9eeef3fadc6d02b4
meetshah1357 <135212738+meetshah1357@users.noreply.github.com>
2023-06-03 20:49:58 +0100

    Add files via upload

---
+++ mmm03062023/src/main/java/alpha.java
@@ -0,0 +1,11 @@
+public class alpha {
+    int tiger = 1;
+    int lion = 2;
+    int cat = 4;
+
+    public static void main(String[] args) {
+        System.out.println("my name is meet");
+        System.out.println("my name is meet");
+        System.out.println("fuck u bitch");
+    }
+}
